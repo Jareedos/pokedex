99bdc8a3bbe64fcd584b90dcd8c9443629ba9254
Jareedos <jaredallens@gmail.com>
2016-06-16 16:49:47 -0700

    added custom Cell View in PokeCell file

---
+++ pokedex/PokeCell.swift
@@ -0,0 +1,25 @@
+//
+//  PokeCell.swift
+//  pokedex
+//
+//  Created by Jared Sobol on 6/16/16.
+//  Copyright © 2016 Appmaker. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+}
