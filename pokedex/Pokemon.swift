2ed008c0b2b850b6e27fb770c3279e1af342bb5b
Jareedos <jaredallens@gmail.com>
2016-06-16 11:49:53 -0700

    Added Model folder & Pokemon File and Class to file

---
+++ pokedex/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by Jared Sobol on 6/16/16.
+//  Copyright © 2016 Appmaker. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
